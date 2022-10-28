07e11af09c88ebcfff872651308e2e5a8c5ce424
sroomberg <stevenroomberg@gmail.com>
2022-10-28 17:09:04 -0400

    Build out base classes

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="packerpy",
+    version="0.0.1",
+    author="sroomberg",
+    author_email="stevenroomberg@gmail.com",
+    description="Packer for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sroomberg/packerpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    packages=[
+        "client",
+        "exceptions",
+        "models",
+        "utils",
+    ],
+    install_requires=[
+        'python-hcl2==3.0.5',
+    ],
+)
