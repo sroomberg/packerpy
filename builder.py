f358284e9cf908cc1748341b25d370baa11ad883
sroomberg <stevenroomberg@gmail.com>
2022-10-28 17:27:27 -0400

    Updates to builder

--- builder.py
+++ builder.py
@@ -5,11 +5,38 @@ from .models import *
 
 
 class PackerBuilder:
-    def __init__(self, name, config_file="packer-builder.pkr.json"):
+    def __init__(self, name, config_file="packer-builder.pkr.json", manifest_file="packer-manifest.json"):
         self.log: logging.Logger = logging.getLogger(PackerBuilder.__name__)
         self.config: PackerConfig = PackerConfig(name, self.log)
         self.config_file: str = config_file
+        self.manifest_file: str = manifest_file
         self.client: PackerClient = PackerClient(self.config_file, log=self.log)
 
+    def artifact_exists(self):
+        if os.path.exists(self.manifest_file):
+            with open(self.manifest_file, "r") as fp:
+                manifest = json.load(fp)
+            return manifest["builds"][0].get("artifact_id", None) is not None
+        return False
+
+    def add_manifest_post_processor(self):
+        for post_processor in self.config.builder.post_processors:
+            if isinstance(post_processor, Manifest):
+                return post_processor.output
+        self.config.builder.add_post_processor(Manifest(self.manifest_file))
+
+    def configure(self):
+        raise NotImplementedError
+
     def build(self):
-        pass
+        self.add_manifest_post_processor()
+        self.client.run("init")
+        self.client.run("build")
+        self.log.info(f"Checking manifest {self.manifest_file} for created artifact(s)")
+        if not self.artifact_exists():
+            raise PackerBuildError(f"Artifact does not exist. Validate file {self.config_file} and rerun with debug for more details.")
+
+    def run(self):
+        self.configure()
+        self.build()
+
