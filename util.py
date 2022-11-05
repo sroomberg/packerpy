d57442098acc21b20251c48c3809ff590eb9af4b
sroomberg <stevenroomberg@gmail.com>
2022-11-05 10:16:36 -0400

    fix project struct, add toml fields, fix setup.py

--- util.py
+++
@@ -1,8 +0,0 @@
-
-def parse_list(raw, delimiter=","):
-    if isinstance(raw, list):
-        return raw
-    elif isinstance(raw, str):
-        return raw.split(delimiter)
-    else:
-        raise ValueError(f"Invalid type {type(raw)}. Only list or str allowed.")
